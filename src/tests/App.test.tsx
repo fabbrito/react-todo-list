ecfbaf0d90a15bfc35b4d650ec4e333a2d0218d6
Fabrício <fab.brito94@gmail.com>
2022-04-24 12:27:17 -0300

    Tests for Modal and for adding new todos

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,9 +1,46 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { App } from '../screens/App';
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { App } from "../screens/App";
+import { Modal } from "../components/Modal";
 
-test('Renders "My Todos"', () => {
+test("Renders 'My Todos' and 'Add Todos'", async () => {
   render(<App />);
-  const text = screen.getByText(/My Todos/i);
-  expect(text).toBeInTheDocument();
+
+  // Check static text
+  const text1 = screen.getByText(/My Todos/i);
+  const text2 = screen.getByText(/Add Todos/i);
+  expect(text1).toBeInTheDocument();
+  expect(text2).toBeInTheDocument();
+});
+
+test("Click 'Add' to create new todo element", async () => {
+  render(<App />);
+
+  fireEvent.click(screen.getByRole("button", { name: /Add/i }));
+  fireEvent.click(screen.getByRole("button", { name: /Add/i }));
+
+  // Wait for page to update with query text
+  const items = await screen.findAllByText(/id: /i);
+  expect(items).toHaveLength(2);
+});
+
+// Component checks
+test("Checks Modal component", () => {
+  const testMessage = "THIS TEST!";
+  const handleClose = jest.fn();
+
+  render(<Modal text={testMessage} onConfirm={handleClose} />);
+
+  // Checks if the messages appears on the modal
+  expect(screen.queryByText(testMessage)).toBeNull();
+
+  // Since no onCancel function was provided, the button should be disabled
+  expect(screen.getByRole("button", { name: /Cancel/i })).toBeDisabled();
+
+  expect(screen.getByRole("button", { name: /Confirm/i })).not.toBeDisabled();
+
+  fireEvent.click(screen.getByRole("button", { name: /Confirm/i }));
+
+  expect(handleClose).toHaveBeenCalledTimes(1);
+
 });
