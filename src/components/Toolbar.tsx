e2609c9f57c58ee57ef92d66ccd5835a95a1983f
Fabrício <fab.brito94@gmail.com>
2022-04-27 07:43:10 -0300

    Toolbar now acts as a navbar

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,23 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Modal } from "./Modal";
 import { Backdrop } from "./Backdrop";
 // A SVG can be manipulated as a ReactComponent
 import { ReactComponent as GarbageIcon } from "../img/garbageIcon.svg";
 
 interface Props {
-  onDeleteAll?: () => void;
+  onDeleteAll?: () => Promise<void>;
   searchFunction?: (text: string) => void;
   onCheckAll?: (isChecked: boolean) => void;
 }
 
 export const Toolbar: React.FC<Props> = ({
   searchFunction = () => {},
-  onDeleteAll = () => {},
+  onDeleteAll = async () => {},
   onCheckAll = () => {},
 }) => {
   const [isChecked, setIsChecked] = useState<boolean>(false);
   const [inputField, setInputField] = useState<string>("");
   const [modalIsOpen, setModalIsOpen] = useState<boolean>(false);
+  const [clearSearch, setClearSearch] = useState<boolean>(false);
 
   const checkboxHandler: React.ChangeEventHandler<HTMLInputElement> = (
     event
@@ -26,14 +27,10 @@ export const Toolbar: React.FC<Props> = ({
     setIsChecked(!isChecked);
   };
 
-  // const onClearSearch: React.MouseEventHandler<HTMLSpanElement> = (event) => {
-  //   setInputField("");
-  //   searchFunction("");
-  // };
-
   const inputHandler: React.ChangeEventHandler<HTMLInputElement> = (event) => {
     setInputField(event.target.value);
     searchFunction(event.target.value);
+    if (clearSearch) setClearSearch(false);
   };
 
   const openModalToDelete: React.MouseEventHandler<SVGSVGElement> = (event) => {
@@ -48,15 +45,24 @@ export const Toolbar: React.FC<Props> = ({
 
   const deleteHandler: React.MouseEventHandler<HTMLButtonElement> = (event) => {
     // Calls the passed function to delete the todo component with matching "id"
-    onDeleteAll();
     setIsChecked(false);
     setModalIsOpen(false);
+    onDeleteAll().then(() => {
+      setClearSearch(true);
+    });
   };
 
   // useEffect(() => {onCheckAll(isChecked)}, [isChecked]);
 
+  useEffect(() => {
+    if (!clearSearch) return;
+    setClearSearch(false);
+    setInputField("");
+    searchFunction("");
+  }, [clearSearch, searchFunction]);
+
   return (
-    <div className="toolbar">
+    <div className="toolbar-container">
       <input
         type="checkbox"
         className="checkbox"
@@ -71,10 +77,7 @@ export const Toolbar: React.FC<Props> = ({
         value={inputField}
         placeholder="Search in todos ..."
       />
-      {/* <span className="clear-search" onClick={onClearSearch}>
-        &times;
-      </span> */}
-      {/* <GarbageIcon className="garbage-icon search" onClick={onClearSearch} /> */}
+
       {modalIsOpen && (
         <Modal
           text={"Delete all checked todos?"}
