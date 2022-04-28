d0d10a98df03dccc323baab3b0665484c82b9a37
Fabrício <fab.brito94@gmail.com>
2022-04-28 05:52:53 -0300

    Modal now in a single component

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,29 +1,39 @@
 import React from "react";
 
-interface Props {
-  text: string;
-  onCancel?: React.MouseEventHandler<HTMLButtonElement>;
+interface ModalProps {
+  text?: string
+  onCancel?: React.MouseEventHandler<HTMLButtonElement | HTMLDivElement>;
   onConfirm?: React.MouseEventHandler<HTMLButtonElement>;
+  children?: React.ReactNode;
 }
 
-export const Modal: React.FC<Props> = ({
-  text,
+export const Modal: React.FC<ModalProps> = ({
+  children,
   onCancel,
   onConfirm,
+  text,
 }) => {
   return (
-    <div className="modal">
-      <p>{text}</p>
-      <button
-        className="btn btn--alt"
-        onClick={onCancel}
-        disabled={onCancel == null}
-      >
-        Cancel
-      </button>
-      <button className="btn" onClick={onConfirm} disabled={onConfirm == null}>
-        Confirm
-      </button>
+    <div className="modal-container">
+      <div className="modal">
+        {text && <p>{text}</p>}
+        {children}
+        <button
+          className="btn btn--alt"
+          onClick={onCancel}
+          disabled={onCancel == null}
+        >
+          Cancel
+        </button>
+        <button
+          className="btn"
+          onClick={onConfirm}
+          disabled={onConfirm == null}
+        >
+          Confirm
+        </button>
+      </div>
+      <div className="backdrop" onClick={onCancel}></div>
     </div>
   );
 };
